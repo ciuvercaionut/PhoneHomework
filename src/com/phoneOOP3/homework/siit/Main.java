b65089556190f8b47464c4422fd26c91f6260dce
ciuvercaionut <ciuvercaionut@gmail.com>
2018-11-22 19:29:56 +0200

    Ultima

--- src/com/phoneOOP3/homework/siit/Main.java
+++ src/com/phoneOOP3/homework/siit/Main.java
@@ -9,14 +9,14 @@ public class Main {
          phone.getImei();
 
          phone.addContact(1,"0755070628","Ionut", "Ciuverca");
-         phone.addContact(2,"0745217489","Iakab","Paul");
+         phone.addContact(2,"0745217489","Neli","Iakab");
 
          phone.listContacts();
 
          phone.sendMessage("0755070628", "Salut!");
          phone.sendMessage("0755070628", "Ce faci?");
          phone.sendMessage(phone.getContacts().get(0), "Totul e ok");
-         phone.sendMessage(phone.getContacts().get(1), "Avem meci azi");
+         phone.sendMessage(phone.getContacts().get(1), "Hai la masa");
 
          phone.listMessages("0755070628");
          phone.listMessages("0745217489");
